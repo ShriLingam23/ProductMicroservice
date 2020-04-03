423f99962cff9eb264cbc480c199f89ca85cb4f6
Shriram Navaratnalingam <it17706438@my.sliit.lk>
2020-04-03 20:17:18 +0530

    Dependency Injection of RepositoryService

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using ProductMicroservice.DBContext;
+using ProductMicroservice.Repository;
 
 namespace ProductMicroservice
 {
@@ -28,6 +29,7 @@ namespace ProductMicroservice
         {
             services.AddControllers();
             services.AddDbContext<ProductContext>(o => o.UseSqlServer(Configuration.GetConnectionString("ProductDB")));
+            services.AddTransient<IProductRepository, ProductRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
