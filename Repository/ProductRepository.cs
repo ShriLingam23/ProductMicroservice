6869ad803d6f65702585355e65b2027d1f03cda4
Shriram Navaratnalingam <it17706438@my.sliit.lk>
2020-04-03 20:15:37 +0530

    Product repository crud implementation

---
+++ Repository/ProductRepository.cs
@@ -0,0 +1,73 @@
+﻿using ProductMicroservice.DBContext;
+using ProductMicroservice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMicroservice.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext _context;
+
+        public ProductRepository(ProductContext context)
+        {
+            this._context = context;
+        }
+
+        public void DeleteProduct(int productId)
+        {
+            var product = _context.Products.Find(productId);
+            _context.Products.Remove(product);
+            Save();
+        }
+
+        public Product GetProductByID(int productId)
+        {
+            return _context.Products.Find(productId);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+        public void InsertProduct(Product product)
+        {
+            _context.Add(product);
+            Save();
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            //There are 3 ways of updating, 
+            // Entry() method in case you want to update all the fields in your object.
+
+            //1. 
+            //  _context.Entry(product).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            //  Save();
+
+            //2.
+            //  var oldProd = _context.Products.Find(product.Id);
+            //  _context.Entry(oldProd).CurrentValues.SetValues(product);
+            //  Save();
+
+            //3.
+            var dbProdEntry = _context.Entry(product);
+            foreach(var property in dbProdEntry.OriginalValues.Properties)
+            {
+                var original = dbProdEntry.OriginalValues.GetValue<object>(property);
+                var current = dbProdEntry.CurrentValues.GetValue<object>(property);
+                if(original != null && !original.Equals(current))
+                    dbProdEntry.Property(property.Name).IsModified = true;
+            }
+            Save();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ProductMicroservice.DBContext;
 
 namespace ProductMicroservice
 {
@@ -25,6 +27,7 @@ namespace ProductMicroservice
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ProductContext>(o => o.UseSqlServer(Configuration.GetConnectionString("ProductDB")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
